feb8c19988ad26065a49a77562cfb4a8bf11e4d1
Cannibal Turtle <lostbunny98@gmail.com>
2025-04-25 14:26:21 +0800

    Update bot_paid_chapters.py

--- bot_paid_chapters.py
+++ bot_paid_chapters.py
@@ -89,13 +89,13 @@ async def send_new_paid_entries():
 
 
             embed = Embed(
-                title=f"**{chaptername}**",
+                title=f"**{chaptername}⋆. 𐙚 ˚**",
                 url=link,
                 description=nameextend or discord.Embed.Empty,
                 timestamp=timestamp,
                 color=int("A87676", 16),
             )
-            embed.set_author(name=f"{translator}⋆. 𐙚")
+            embed.set_author(name=f"{translator}˙ᵕ˙")
             if thumb_url:
                 embed.set_thumbnail(url=thumb_url)
             embed.set_footer(text=host, icon_url=host_logo)
