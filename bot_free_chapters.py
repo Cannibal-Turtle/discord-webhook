f063d7cdf69641f334748d5f2c6dac8b133992a4
Cannibal Turtle <lostbunny98@gmail.com>
2025-04-26 20:26:49 +0800

    Update bot_free_chapters.py

--- bot_free_chapters.py
+++ bot_free_chapters.py
@@ -98,7 +98,7 @@ async def send_new_entries():
             timestamp   = dateparser.parse(pubdate_raw) if pubdate_raw else None
 
             embed = Embed(
-                title=f"**{chaptername}**<a:MoonSparkles:1365568607119085578>",
+                title=f"**{chaptername}**<a:moonandstars:1365569468629123184>",
                 url=link,
                 description=nameextend or discord.Embed.Empty,
                 timestamp=timestamp,
