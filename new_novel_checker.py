6cb153c94d26d095f8966df5664f43a8ec2eee9d
Cannibal Turtle <lostbunny98@gmail.com>
2026-06-22 03:18:38 +0800

    Update new_novel_checker.py

--- new_novel_checker.py
+++ new_novel_checker.py
@@ -52,8 +52,8 @@ STATE_PATH     = require_file_value("state_path")
 BOT_TOKEN_ENV  = "DISCORD_BOT_TOKEN"
 CHANNEL_ID_ENV = "DISCORD_CHANNEL_ID"
 
-GLOBAL_ROLE = require_role_value("new")
-NSFW_ROLE   = require_role_value("nsfw")
+GLOBAL_ROLE = role_id_to_mention(require_role_value("new"))
+NSFW_ROLE   = role_id_to_mention(require_role_value("nsfw"))
 
 TAG_ROLE_MAP_PATH = require_file_value("tag_role_map_file")
 # ───────────────────────────────────────────────────────────────────────────────
